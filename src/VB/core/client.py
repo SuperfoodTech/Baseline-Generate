e55bfed62e43ab91dedbc0e0cfaa3eef07afe9a2
Akbar <mochakbarhanafi@gmail.com>
2026-06-06 13:39:03 +0700

    chore: remove junk directories, test files, and clean up chrome profiles version control tracking

--- src/VB/core/client.py
+++
@@ -1,7 +0,0 @@
-"""
-src/VB/core/client.py
-=====================
-Re-exports ShopeeClient from the root automation core.
-"""
-
-from core.client import ShopeeClient, build_img_url
--- src/VB/core/logger.py
+++
@@ -1,7 +0,0 @@
-"""
-src/VB/core/logger.py
-=====================
-Re-exports get_logger from the root automation core.
-"""
-
-from core.logger import get_logger
